a0f8bfd332ec19c3895567ed6d41e2905718c5eb
Cyril-lavoisier <45697597+Cyril-lavoisier@users.noreply.github.com>
2022-11-02 21:49:11 +0100

    Add files via upload

--- map_0.php
+++ map_0.php
@@ -9,15 +9,76 @@
 </head>
 
 <body>
-<?php
-    $dir    = './texture_map';
-    $files1 = scandir($dir);
-    foreach ($files1 as $key => $value) {
-?>
-<img src="./texture_map/<?php echo $value; ?>" class="draggable">
-<?php
-    }
-?>
+<div id="menu">
+    <ul>
+        <li><button class="btn">Environnement</button></li>
+        <li><button class="btn">Decoration</button></li>
+        <li><button class="btn">Maison exterieur</button></li>
+        <li><button class="btn">Maison interieur</button></li>
+        <li><input type="checkbox" id="tiles_par_tiles" name="tiles_par_tiles" onclick="tiles_par_tiles()"><label for="tiles_par_tiles">Integration tiles par tiles</label></li>
+        <li><input type="checkbox" id="tiles_continue" name="tiles_continue" onclick="tiles_continue()"><label for="tiles_continue">Integration continue</label></li>
+        <li><button>Effacer tiles</button></li>
+        <li><button>Enregistrer le fichier</button></li>
+    </ul>
+</div>
+
+<div id="myModal" class="modal">
+    <div class="modal-content">
+        <span class="close">&times;</span>
+        <h1>Environnement</h1>
+        <?php
+            $dir    = './texture_map';
+            $files1 = scandir($dir);
+            foreach ($files1 as $key => $value) {
+        ?>
+        <img src="./texture_map/<?php echo $value; ?>" class="texture">
+        <?php
+            }
+        ?>
+    </div>
+</div>
+<div id="myModal" class="modal">
+    <div class="modal-content">
+        <span class="close">&times;</span>
+        <p>Decoration</p>
+    </div>
+</div>
+<div id="myModal" class="modal">
+    <div class="modal-content">
+        <span class="close">&times;</span>
+        <p>Maison exterieur</p>
+    </div>
+</div>
+<div id="myModal" class="modal">
+    <div class="modal-content">
+        <span class="close">&times;</span>
+        <p>Maison interieur</p>
+    </div>
+</div>
+<div id="myModal" class="modal">
+    <div class="modal-content">
+        <span class="close">&times;</span>
+        <p>Integration Tiles / Tiles</p>
+    </div>
+</div>
+<div id="myModal" class="modal">
+    <div class="modal-content">
+        <span class="close">&times;</span>
+        <p>integration continue</p>
+    </div>
+</div>
+<div id="myModal" class="modal">
+    <div class="modal-content">
+        <span class="close">&times;</span>
+        <p>Effacer tiles</p>
+    </div>
+</div>
+<div id="myModal" class="modal">
+    <div class="modal-content">
+        <span class="close">&times;</span>
+        <p>Enregistrer le fichier</p>
+    </div>
+</div>
     <div id="tray">
         <?php for ($i=0; $i < 1652; $i++) { ?>
         <div id="<?php echo $i ?>" class="case"></div>
@@ -26,7 +87,7 @@
         ?>
     </div>
     <script src="./jquery-3.6.1.min.js"></script>
-    <!-- <script src="./jquery-ui.js"></script> -->
+    <script src="./jquery-ui.js"></script>
     <script src="./script.js"></script>
 </body>
 </html>
--- script.js
+++ script.js
@@ -1,29 +1,58 @@
 let selected_texture = "";
-console.log(selected_texture);
 
-$(".draggable").click(function (e) {
+//Quand on selectionne une texture
+$(".texture").click(function (e) {
     e.preventDefault();
     selected_texture = $(this).attr("src");
-    $(".draggable").css("background-color", "");
+    $(".texture").css("background-color", "");
     $(this).css("background-color", "red");
-    $(".draggable").css("cursor", "pointer");
 });
 
 //Fonction remplir toute les case
-$(".case").hover(function () {
-    selected_case = $(this).attr("id");
-    if (selected_texture != "") {
-        $(this).html("<img src='" + selected_texture + "'>");
-    }
-});
-
-$(".case").click(function (e) {
-    e.preventDefault();
-    console.log(selected_texture);
-    if (selected_texture != "") {
-        $(this).html("<img src='" + selected_texture + "'>");
-        $(".draggable").css("background-color", "white");
-        $(".case").css("cursor", "");
+function tiles_continue() {
+    $(".case").hover(function () {
+        if (selected_texture != "" && document.getElementById("tiles_continue").checked == true) {
+            $(this).html("<img src='" + selected_texture + "'>");
+        }
+    });
+    /*
+    if (document.getElementById("tiles_continue").checked == false) {
         selected_texture = "";
     }
+    */
+};
+
+//Fonction case par case
+function tiles_par_tiles() {
+    $(".case").click(function (e) {
+        e.preventDefault();
+        if (selected_texture != "" && document.getElementById("tiles_par_tiles").checked == true) {
+            $(this).html("<img src='" + selected_texture + "'>");
+            $(".case").css("cursor", "");
+        }
+    });
+    /*
+    if (document.getElementById("tiles_par_tiles").checked == false) {
+        selected_texture = "";
+    }
+    */
+};
+
+/* MODAL MENU */
+let btn = document.querySelectorAll(".btn");
+let modal = document.querySelectorAll('#myModal');
+let span = document.querySelectorAll(".close");
+
+[...btn].forEach((btn, ind) => {
+    btn.onclick = () => (modal[ind].style.display = "block");
+});
+
+[...span].forEach((span, ind) => {
+    span.onclick = () => (modal[ind].style.display = "none");
+    selected_texture = "";
+});
+
+
+$(function() {
+    $( ".modal" ).draggable();
 });
