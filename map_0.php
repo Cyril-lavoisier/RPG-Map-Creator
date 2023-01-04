602ff7d73671423602e4c7101705a6ab5838c5ae
Cyril-lavoisier <45697597+Cyril-lavoisier@users.noreply.github.com>
2023-01-04 12:37:08 +0100

    Add files via upload

--- map_0.php
+++ map_0.php
@@ -9,83 +9,92 @@
 </head>
 
 <body>
-<div id="menu">
-    <ul>
-        <li><button class="btn">Environnement</button></li>
-        <li><button class="btn">Decoration</button></li>
-        <li><button class="btn">Maison exterieur</button></li>
-        <li><button class="btn">Maison interieur</button></li>
-        <li><input type="checkbox" id="tiles_par_tiles" name="tiles_par_tiles" onclick="tiles_par_tiles()"><label for="tiles_par_tiles">Integration tiles par tiles</label></li>
-        <li><input type="checkbox" id="tiles_continue" name="tiles_continue" onclick="tiles_continue()"><label for="tiles_continue">Integration continue</label></li>
-        <li><button>Effacer tiles</button></li>
-        <li><button>Enregistrer le fichier</button></li>
-    </ul>
-</div>
+    <div id="menu">
+        <ul>
+            <li><button class="btn">Environnement</button></li>
+            <li><button class="btn">Decoration</button></li>
+            <li><button class="btn">Maison exterieur</button></li>
+            <li><button class="btn">Maison interieur</button></li>
+            <li><input type="checkbox" id="tiles_par_tiles" name="tiles_par_tiles" onclick="tiles_par_tiles()"><label for="tiles_par_tiles">Integration tiles par tiles</label></li>
+            <li><input type="checkbox" id="tiles_continue" name="tiles_continue" onclick="tiles_continue()"><label for="tiles_continue">Integration continue</label></li>
+            <li><button class="btn">Collision</button></li>
+        </ul>
+    </div>
 
-<div id="myModal" class="modal">
-    <div class="modal-content">
-        <span class="close">&times;</span>
-        <h1>Environnement</h1>
-        <?php
+    <div id="game_modal" class="modal">
+        <div class="modal-content">
+            <span class="close">&times;</span>
+            <h1>Environnement</h1>
+            <?php
             $dir    = './texture_map';
             $files1 = scandir($dir);
             foreach ($files1 as $key => $value) {
-        ?>
-        <img src="./texture_map/<?php echo $value; ?>" class="texture">
-        <?php
+            ?>
+                <img src="./texture_map/<?php echo $value; ?>" class="texture">
+            <?php
             }
-        ?>
+            ?>
+        </div>
     </div>
-</div>
-<div id="myModal" class="modal">
-    <div class="modal-content">
-        <span class="close">&times;</span>
-        <p>Decoration</p>
+    <div id="game_modal" class="modal">
+        <div class="modal-content">
+            <span class="close">&times;</span>
+            <h1>Decoration</h1>
+            <?php
+            $dir    = './deco_map';
+            $files1 = scandir($dir);
+            foreach ($files1 as $key => $value) {
+            ?>
+                <img src="./deco_map/<?php echo $value; ?>" class="deco">
+            <?php
+            }
+            ?>
+        </div>
     </div>
-</div>
-<div id="myModal" class="modal">
-    <div class="modal-content">
-        <span class="close">&times;</span>
-        <p>Maison exterieur</p>
+    <div id="game_modal" class="modal">
+        <div class="modal-content">
+            <span class="close">&times;</span>
+            <p>Maison exterieur</p>
+        </div>
     </div>
-</div>
-<div id="myModal" class="modal">
-    <div class="modal-content">
-        <span class="close">&times;</span>
-        <p>Maison interieur</p>
+    <div id="game_modal" class="modal">
+        <div class="modal-content">
+            <span class="close">&times;</span>
+            <p>Maison interieur</p>
+        </div>
     </div>
-</div>
-<div id="myModal" class="modal">
-    <div class="modal-content">
-        <span class="close">&times;</span>
-        <p>Integration Tiles / Tiles</p>
+    <div id="game_modal" class="modal">
+        <div class="modal-content">
+            <span class="close">&times;</span>
+            <p>Collision</p>
+            <ul>
+                <li><input type="checkbox" id="ch" name="ch" onclick="collision(ch)"><label for="ch">Collision Haut</label></li>
+                <li><input type="checkbox" id="cb" name="cb" onclick="collision(cb)"><label for="cb">Collision Bas</label></li>
+                <li><input type="checkbox" id="cd" name="cd" onclick="collision(cd)"><label for="cd">Collision Droit</label></li>
+                <li><input type="checkbox" id="cg" name="cg" onclick="collision(cg)"><label for="cg">Collision Gauche</label></li>
+                <li><input type="checkbox" id="chd" name="chd" onclick="collision(chd)"><label for="chd">Collision Haut Droit</label></li>
+                <li><input type="checkbox" id="cbd" name="cbd" onclick="collision(cbd)"><label for="cbd">Collision Bas Droit</label></li>
+                <li><input type="checkbox" id="chg" name="chg" onclick="collision(chg)"><label for="chg">Collision Haut Gauche</label></li>
+                <li><input type="checkbox" id="cbg" name="cbg" onclick="collision(cbg)"><label for="cbg">Collision Bas Gauche</label></li>
+            </ul>
+        </div>
     </div>
-</div>
-<div id="myModal" class="modal">
-    <div class="modal-content">
-        <span class="close">&times;</span>
-        <p>integration continue</p>
-    </div>
-</div>
-<div id="myModal" class="modal">
-    <div class="modal-content">
-        <span class="close">&times;</span>
-        <p>Effacer tiles</p>
-    </div>
-</div>
-<div id="myModal" class="modal">
-    <div class="modal-content">
-        <span class="close">&times;</span>
-        <p>Enregistrer le fichier</p>
-    </div>
-</div>
     <div id="tray">
-        <?php for ($i=0; $i < 1652; $i++) { ?>
-        <div id="<?php echo $i ?>" class="case"></div>
+        <?php for ($i = 0; $i < 1652; $i++) { ?>
+            <div id="<?php echo $i ?>" class="case"></div>
         <?php
         }
         ?>
-    </div>
+    </div><br>
+    <div id="dimensional_tray">
+        <?php for ($i = 0; $i < 1652; $i++) { ?>
+            <div class="dimensional_case_<?php echo $i ?>">'X',</div>
+        <?php
+        }
+        ?>
+    </div><br>
+    <!-- <div id="generated_css"></div> -->
+    <div id="tray_event"></div><br>
     <script src="./jquery-3.6.1.min.js"></script>
     <script src="./jquery-ui.js"></script>
     <script src="./script.js"></script>
--- script.js
+++ script.js
@@ -1,46 +1,44 @@
+//Tableau représentant une ligne, chaque lettre représente une case
+let line0 = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'] + "<br>"
+let line1 = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'] + "<br>"
+let line2 = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'] + "<br>"
+let line3 = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'] + "<br>"
+let line4 = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'] + "<br>"
+let line5 = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'] + "<br>"
+let line6 = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'] + "<br>"
+let line7 = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'] + "<br>"
+let line8 = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'] + "<br>"
+let line9 = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'] + "<br>"
+let line10 = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'] + "<br>"
+let line11 = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'] + "<br>"
+let line12 = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'] + "<br>"
+let line13 = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'] + "<br>"
+let line14 = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'] + "<br>"
+let line15 = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'] + "<br>"
+let line16 = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'] + "<br>"
+let line17 = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'] + "<br>"
+let line18 = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'] + "<br>"
+let line19 = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'] + "<br>"
+let line20 = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'] + "<br>"
+let line21 = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'] + "<br>"
+let line22 = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'] + "<br>"
+let line23 = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'] + "<br>"
+let line24 = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'] + "<br>"
+let line25 = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'] + "<br>"
+let line26 = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'] + "<br>"
+let line27 = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'] + "<br>"
+
+
+let map = [line0, line1, line2, line3, line4, line5, line6, line7, line8, line9, line10, line11, line12, line13, line14, line15, line16, line17, line18, line19, line20, line21, line22, line23, line24, line25, line26, line27] //Tableau a deux dimension représentant toute la map
+let tray_event = document.querySelector("#tray_event");
+//tray_event.innerHTML = map;
+
+
 let selected_texture = "";
-
-//Quand on selectionne une texture
-$(".texture").click(function (e) {
-    e.preventDefault();
-    selected_texture = $(this).attr("src");
-    $(".texture").css("background-color", "");
-    $(this).css("background-color", "red");
-});
-
-//Fonction remplir toute les case
-function tiles_continue() {
-    $(".case").hover(function () {
-        if (selected_texture != "" && document.getElementById("tiles_continue").checked == true) {
-            $(this).html("<img src='" + selected_texture + "'>");
-        }
-    });
-    /*
-    if (document.getElementById("tiles_continue").checked == false) {
-        selected_texture = "";
-    }
-    */
-};
-
-//Fonction case par case
-function tiles_par_tiles() {
-    $(".case").click(function (e) {
-        e.preventDefault();
-        if (selected_texture != "" && document.getElementById("tiles_par_tiles").checked == true) {
-            $(this).html("<img src='" + selected_texture + "'>");
-            $(".case").css("cursor", "");
-        }
-    });
-    /*
-    if (document.getElementById("tiles_par_tiles").checked == false) {
-        selected_texture = "";
-    }
-    */
-};
-
-/* MODAL MENU */
+let selected_deco = "";
+//Modal menu
 let btn = document.querySelectorAll(".btn");
-let modal = document.querySelectorAll('#myModal');
+let modal = document.querySelectorAll('#game_modal');
 let span = document.querySelectorAll(".close");
 
 [...btn].forEach((btn, ind) => {
@@ -52,7 +50,129 @@ let span = document.querySelectorAll(".close");
     selected_texture = "";
 });
 
-
-$(function() {
-    $( ".modal" ).draggable();
+//Drag des modal
+$(function () {
+    $(".modal").draggable();
 });
+
+//Quand on selectionne une texture
+$(".texture").click(function (e) {
+    e.preventDefault();
+    selected_texture = $(this).attr("src");
+    $(".texture").css("background-color", "");
+    $(this).css("background-color", "red");
+});
+
+/*Quand on selectionne une texture
+let generated_css = document.querySelector("#generated_css");
+$(".deco").click(function (e) {
+    e.preventDefault();
+    selected_deco = $(this).attr("src");
+    $(".deco").css("background-color", "");
+    $(this).css("background-color", "blue");
+    var newSpan = document.createElement('span');
+    var newBr = document.createElement('br');
+    generated_css.appendChild(newSpan);
+    generated_css.appendChild(newBr);
+    var text = document.createTextNode(selected_deco);
+    newSpan.appendChild(text);
+});
+*/
+
+//Fonction remplir toute les case
+function tiles_continue() {
+    $(".case").hover(function () {
+        if (selected_texture != "" && document.getElementById("tiles_continue").checked == true) {
+            $(this).html("<img src='" + selected_texture + "'>");
+        }
+    });
+};
+
+//Fonction case par case
+function tiles_par_tiles() {
+    $(".case").click(function (e) {
+        e.preventDefault();
+        if (selected_texture != "" && document.getElementById("tiles_par_tiles").checked == true) {
+            $(this).html("<img src='" + selected_texture + "'>");
+            $(".case").css("cursor", "");
+        }
+    });
+};
+
+//Fonction collision
+function collision(target) {
+    $(".case").click(function (e) {
+        let type_collision = target.name;
+        let id_case = e.target.id;
+        switch (type_collision) {
+            case "ch":
+                e.preventDefault();
+                if (document.getElementById("ch").checked == true) {
+                    $(this).addClass("ch");
+                    $(".dimensional_case_"+id_case).text("'CH',");
+                    $(".case").css("cursor", "");
+                }
+                break;
+            case "cb":
+                e.preventDefault();
+                if (document.getElementById("cb").checked == true) {
+                    $(this).addClass("cb");
+                    $(".dimensional_case_"+id_case).text("'CB',");
+                    $(".case").css("cursor", "");
+                }
+                break;
+            case "cd":
+                e.preventDefault();
+                if (document.getElementById("cd").checked == true) {
+                    $(this).addClass("cd");
+                    $(".dimensional_case_"+id_case).text("'CD',");
+                    $(".case").css("cursor", "");
+                }
+                break;
+            case "cg":
+                e.preventDefault();
+                if (document.getElementById("cg").checked == true) {
+                    $(this).addClass("cg");
+                    $(".dimensional_case_"+id_case).text("'CG',");
+                    $(".case").css("cursor", "");
+                }
+                break;
+            case "chd":
+                e.preventDefault();
+                if (document.getElementById("chd").checked == true) {
+                    $(this).addClass("chd");
+                    $(".dimensional_case_"+id_case).text("'CHD',");
+                    $(".case").css("cursor", "");
+                }
+                break;
+            case "cbd":
+                e.preventDefault();
+                if (document.getElementById("cbd").checked == true) {
+                    $(this).addClass("cbd");
+                    $(".dimensional_case_"+id_case).text("'CBD',");
+                    $(".case").css("cursor", "");
+                }
+                break;
+            case "chg":
+                e.preventDefault();
+                if (document.getElementById("chg").checked == true) {
+                    $(this).addClass("chg");
+                    $(".dimensional_case_"+id_case).text("'CHG',");
+                    $(".case").css("cursor", "");
+                }
+                break;
+            case "cbg":
+                e.preventDefault();
+                if (document.getElementById("cbg").checked == true) {
+                    $(this).addClass("cbg");
+                    $(".dimensional_case_"+id_case).text("'CBG',");
+                    $(".case").css("cursor", "");
+                }
+                break;
+            default:
+                break;
+        }
+    });
+}
+
+
